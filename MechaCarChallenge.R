26239767d02c56f1dcf27efd6d0efe6a33229f3a
Kwas45 <102786356+Kwas45@users.noreply.github.com>
2022-07-24 01:25:50 -0700

    Updaing challenge file.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,7 +13,6 @@ lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clea
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = Car_mpg))
 
 
-
 # Deliverable 2
 
 # read Suspension_Coil.csv file
@@ -24,3 +23,19 @@ total_summary <- Susp_coil %>% summarize(Mean = mean(PSI), Median = median(PSI),
 
 # creating lot_summary using group_by() and summarize()
 lot_summary <- Susp_coil %>% group_by (Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+
+# Deliverable 3
+
+# determining difference in PSI for all manufacturing lots using t.test()
+t.test(Susp_coil$PSI,mu = 1500)
+
+# determining difference in PSI for each manufacturing lot using t.test() and subset()
+L1 <- subset(Susp_coil, Manufacturing_Lot == 'Lot1')
+L2 <- subset(Susp_coil, Manufacturing_Lot == 'Lot2')
+L3 <- subset(Susp_coil, Manufacturing_Lot == 'Lot3')
+
+t.test(L1$PSI, mu = 1500) 
+t.test(L2$PSI, mu = 1500)
+t.test(L3$PSI, mu = 1500)
+
